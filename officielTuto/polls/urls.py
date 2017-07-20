4a750d0ed5bf23ba8efe1d7729570b379fa6c293
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-20 21:12:02 +0200

    End of Part3 Tuto

--- officielTuto/polls/urls.py
+++ officielTuto/polls/urls.py
@@ -7,6 +7,10 @@ Remember : url(regex, view, kwargs=None, name=None)[source]
 from django.conf.urls import url
 from . import views
 
+# Namespace this urls 
+
+app_name = 'polls'
+
 urlpatterns = [
   # url(r'^login', views.login, name="login"),
   #ex /polls/
@@ -14,6 +18,9 @@ urlpatterns = [
   
   # ex /polls/5/
   url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+
+  #ex /polls/5/details
+  url(r'^(?P<question_id>[0-9]+)/details/$', views.detail2, name="details"),
 
   # ex /polls/5/results/
   url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
--- officielTuto/polls/views.py
+++ officielTuto/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 # from django.template import loader
 from .models import Question
@@ -29,9 +29,16 @@ def detail(request, question_id):
     raise Http404("Question does not exist")
   return render(request, 'polls/detail.html', {'question': question})
 
+
+def detail2(request, question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  return render(request, 'polls/detail.html',{'question': question})
+
 def results(request, question_id):
   response = "You're looking at the resutls of question %s."
   return HttpResponse(response % question_id)
 
 def vote(request,question_id):
   return HttpResponse("You are voting on question %s. "%question_id)
+
+
